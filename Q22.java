17033fe0a42ec29fc7150e7d9cd293d6a2b1178b
KingslayerGeralt <50865596+KingslayerGeralt@users.noreply.github.com>
2019-06-06 17:27:55 +0800

    Add files via upload

--- Q22.java
+++ Q22.java
@@ -1,3 +1,14 @@
+/*
+  上机题Q22
+
+  写出一个满足Singleton模式地类出来
+
+  更新于2019.06.04
+
+  Author：黄亮亮
+
+ */
+
 package TheExam1_2;
 
 public class Q22 {
@@ -9,9 +20,8 @@ public class Q22 {
 
     public static Q22 getInstance(){
         return instance;
-
     }
-
+    //写一个show方法，可以调用
     public void show(){
         System.out.println("Here is Singleton Message");
     }
